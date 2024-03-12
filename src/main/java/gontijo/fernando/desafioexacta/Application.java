344e4dc8d83f8c0c9819b7e74bc6a39dc68286e9
fernandogontijo <fernando.gontijos@hotmail.com>
2024-03-12 11:51:08 -0300

    Ajustes finais

--- src/main/java/gontijo/fernando/desafioexacta/Application.java
+++ src/main/java/gontijo/fernando/desafioexacta/Application.java
@@ -3,9 +3,11 @@ package gontijo.fernando.desafioexacta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan("gontijo.fernando.desafioexacta.model")
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
